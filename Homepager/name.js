d63a87101a91f3198871dbb8c49e35d01a7dc564
anurag27397 <anurag27397@gmail.com>
2017-03-14 19:36:04 +0530

    Homepager
    A chrome extension to customise user "newtab" and makes it look elegant.

---
+++ Homepager/name.js
@@ -0,0 +1,22 @@
+function getUserInfo(interactive) {
+    xhrWithAuth('GET',
+                'https://www.googleapis.com/plus/v1/people/me',
+                interactive,
+                onUserInfoFetched);
+  }
+
+   function onUserInfoFetched(error, status, response) {
+    if (!error && status == 200) {
+      changeState(STATE_AUTHTOKEN_ACQUIRED);
+      sampleSupport.log(response);
+      var user_info = JSON.parse(response);
+      populateUserInfo(user_info);
+    } else {
+      changeState(STATE_START);
+    }
+  }
+
+   function populateUserInfo(user_info) {
+    user_info_div.innerHTML = "Hello " + user_info.displayName;
+}
+    
